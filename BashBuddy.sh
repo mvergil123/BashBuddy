51529d978adb8a18dce7f8de6f0e89a1f45f2b3b
Mike Vergil <110843487+mvergil123@users.noreply.github.com>
2025-06-11 15:24:49 -0600

    Update BashBuddy.sh
    Began adding the full recon function as well as a function that checks for open ports such as 22, 80, 443, etc to be used in dirsearch

--- BashBuddy.sh
+++ BashBuddy.sh
@@ -37,6 +37,43 @@ else    # ping failed :(
         exit 1
 fi
 #-------------------------------------------------------------------------------------------------------------------------
+# Is port open function for dirsearch
+IsPortOpen(){
+        ip="$1"
+        port="$2"
+        nc -z -w1 "$ip" "$port" > /dev/null 2>&1        # check if port is opened
+        return $?       # Return True or False
+}
+#-------------------------------------------------------------------------------------------------------------------------
+# Full Recon Function
+Full_Recon(){
+        echo "Commencing Full Recon. This may take a while...."
+        ip="$1"
+        mkdir -p results/$ip # create results dir if not already created
+
+        # Verbose nmap scan on IP for open ports
+        nmap -vv -sV -p- -oG results/$ip/nmap.grep "$ip"        # verbose nmap scan on all ports and write results to file
+        echo -e "${GREEN}[+] Nmap scan completed${NORMAL}"
+
+        # Check if ports 80 or 443 are opened to utilize dirsearch and curl
+        if IsPortOpen "$ip" 80; then
+                dirsearch -u "http://$ip" -x 403,404 -o "results/$ip/dirsearch_port80.txt"
+                curl -s "http://$ip" >> "results/$ip/dirsearch_port80.txt"
+        fi
+        if IsPortOpen "ip" 443; then
+                dirsearch -u "https://$ip" -x 403,404 -o "results/$ip/dirsearch_port443.txt"
+                curl -sk "https://$ip" >> "results/$ip/dirsearch_port443.txt"
+        fi
+        echo -e "${GREEN}[+] Dirsearch and curl completed${NORMAL}"
+
+        # Brute force port 22 is opened and any login page
+
+
+
+
+
+}
+#-------------------------------------------------------------------------------------------------------------------------
 # Prompt the menu to the user
 figlet -f small "Menu"
 echo "1) Full Recon (nmap, dirsearch, curl, whois)"
